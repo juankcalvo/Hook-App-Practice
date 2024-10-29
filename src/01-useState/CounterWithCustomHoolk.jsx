ce30275c8b04c065eeccbff7c0248feb36766c4f
juankcalvo <79115866+juankcalvo@users.noreply.github.com>
2024-10-29 17:41:07 -0600

    segunta practica

--- src/01-useState/CounterWithCustomHoolk.jsx
+++ src/01-useState/CounterWithCustomHoolk.jsx
@@ -5,30 +5,28 @@ export const CounterWithCustomHook = () => {
 
   return (
     <div className="container mt-4">
-      {/* Encabezado del contador */}
       <div className="d-flex justify-content-center align-items-center card-header">
         <h1>Counter with Hook: {counter}</h1>
       </div>
 
       <hr />
 
-      {/* Botones alineados horizontalmente */}
       <div className="d-flex justify-content-center align-items-center mt-4">
         <button 
           onClick={() => increment(2)} 
-          className="btn btn-primary mx-2">
+          className="btn btn-outline-primary mx-2">
           +2
         </button>
 
         <button 
           onClick={reset} 
-          className="btn btn-secondary mx-2">
+          className="btn btn-outline-secondary mx-2">
           Reset
         </button>
 
         <button 
           onClick={() => decrement(2)} 
-          className="btn btn-danger mx-2">
+          className="btn btn-outline-danger mx-2">
           -2
         </button>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'
-import { CounterApp } from './01-useState/CounterApp';
+//import { CounterApp } from './01-useState/CounterApp';
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHoolk';
 //import { HooksApp } from './HooksApp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </StrictMode>,
 )
